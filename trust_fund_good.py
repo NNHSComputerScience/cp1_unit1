b72b240e426e5ca2dd2b31bbe541e86f10df55e9
Matt Callaghan <mcallaghan@naperville203.org>
2020-06-03 17:40:17 -0500

    revised for intro to debugging activity

--- trust_fund_good.py
+++ trust_fund_good.py
@@ -16,12 +16,13 @@ Trust Fund Buddy
 Totals your monthly spending so that your trust fund doesn't run out
 (and so you aren't forced to get a real job).
 
-Please enter the requested, monthly costs.  Since you're rich, ignore pennies
+Please enter the requested monthly costs.  Since you're rich, ignore pennies
 and use only whole dollar amounts.
 
 """)
+name = input("What is your name? ")
 
-car = int(input("Lamborghini Tune-Ups: "))
+car = int(input("Lamborghini Tune-Up(s): "))
 rent = int(input("Manhattan Apartment: "))
 jet = int(input("Private Jet Rental: "))
 gifts = int(input("Extravegant Gifts: "))
@@ -32,7 +33,8 @@ games = int(input("Computer Games: "))
 
 total = car + rent + jet + gifts + food + staff + guru + games
 
-print ("\nGrand Total: $" + str(total))
+print()
+print (name.title() + "'s Total Monthly Expenses: $" + str(total))
 
 input("\n\nPress the enter key to exit.")
 
