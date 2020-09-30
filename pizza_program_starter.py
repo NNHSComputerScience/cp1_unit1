42e521a8b6983f4aa1e7b8b048fa67b53f54fe3f
Matt Callaghan <mcallaghan@naperville203.org>
2020-09-30 07:06:42 -0500

    Create pizza_program_starter.py

---
+++ pizza_program_starter.py
@@ -0,0 +1,20 @@
+"""
+# COMPLETE COMMENT HEADER
+Name: 
+Date:
+Title: Pizza Program, pizza_program.py
+Description: Asks the user for how many pizzas they wold like to order
+    and calculates the total cost.  Displays the information in nice format.
+"""
+
+# welcome message to the program, initialize variables, and get user input
+
+
+# cost calculation
+
+
+# display output in a nice format
+
+
+
+input("\n\nPress enter to exit.")
