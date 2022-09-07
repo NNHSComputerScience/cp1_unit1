5c6a2b733c7ebdf13542bc0bcf6ba470489f2611
Matt Callaghan <mcallaghan@naperville203.org>
2022-09-07 15:06:53 -0500

    Update unit1_notes_variables.py

--- unit1_notes_variables.py
+++ unit1_notes_variables.py
@@ -1,50 +1,11 @@
 """
-Name: Mr. Callaghan
-Date:
-Title: Unit 1 Notes - Math, Variables, User Input, & Types
+Name: 
+Title: Math, Variables, User Input, & Types
 Description: Working with fundametal data types and operations in Python. Storing data in variables. Interacting with a user.
 """
 
-# Math operators, variables, user input, & types
-
-# CONCATENATION: Two strings combined into one using the plus sign.
-print ("cup")
-print ("cake")
-print ("cup" + "cake")
-print ()
-
-# string repetition
-print ("happy")
-print ("happy" * 100)    # can't divide or subtract a string
-print ()
-
-# Numbers types in Python:
-#   INTEGER(int) - whole numbers, no decimal point 
-#   FLOATING POINT NUMBER (float) - decimal number
-
-# Math Operators:
-#   + , - , / , //, * , ** , % #CHALLENGE: predict what will happen and run
-print (2+2)
-print (5-2)
-print (19/4)    # true division
-print (19//4)   # integer division - leaves off remainder
-print (2*7)
-print (3**3)    # exponents
-print (11%3)    # modulus - gives the remainder
- 
-# STATEMENT - a complete thought in a programming language (equivalent to one complete line of code)
-#       e.g. print(2+2)
-# EXPRESSION - code that can be evaluated to a new value (like a math operation)
-#       e.g. 2+2
-
-# VARIABLE - a user-defined name in a program, which provides a way to access
-#   information stored in a computer's memory.
-#   Follow all variable naming rules and conventions.
-#   !!! review slides on variables:
-
-name = "Monty"  # read as: assign the string value "Monty" to variable "name"
-
-print(name) 
+# VARIABLE - a user-defined name in a program, which provides a way to access information stored in a computer's memory.
+#   Follow all variable naming rules and conventions:
 
 # Rules for legal variable names:
 #  - Contain only numbers, letters, and underscores
@@ -58,63 +19,83 @@ print(name)
 #     - names that begin with an underscore have a special meaning
 #     - don’t begin with a capital letter 
 #  - Consider the trade-off of long names
+#   !!! review slides on variables:
 
-# ASSIGNMENT STATEMENT: assigns a value to a variable;
-#   creates (INITIALIZES) variable if necessary
-# ASSIGNMENT OPERATOR: = (equals sign); assigns values from right to left ONLY 
+name = "Monty"  # read as: assign the string value "Monty" to variable "name"
 
 # Reassigning to a variable to update its value
 name = "Python" # we can't get "Monty" back!
-print(name)
+#print(name) 
+
+# ASSIGNMENT STATEMENT: assigns a value to a variable; creates (INITIALIZES) variable if necessary
+# ASSIGNMENT OPERATOR: = (equals sign); assigns values from right to left ONLY 
+
+result = "0"	# string
 
 # INPUT function - Pauses program execution and gets typed text from the user of our program. Returns it to you as a string.
 #    Only accepts 1 string argument, which should be a prompt to the user on what to type. User presses 'Enter" when done typing.
 #    Tip: Including a space at the end of your string argument makes the prompt more user-friendly.
-name = input("What is your name? ")
-                              
-num = 7 # int
-num2 = 7.7 # float
-print(name, num, num2) # we can save any type of value in a variable
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
 
-# CHALLENGE: Store the user's first, middle, and last names in 3 separate 
-#       variables and then print the following: "Hello, <FIRST> <MIDDLE> <LAST>!"
+# CONCATENATION: Two strings combined into one using the plus sign.
 
-first = input("Enter your first name: ") 
-middle = input("Enter your middle name: ") 
-last = input("Enter your last name: ") 
+name = first_name + " " + last_name
 
-print("Hello," , first , middle , last + "!")
+print("\nWelcome to the calculator program", name + "!")
 
-# using a comma - inserts a space & separates arguments 
+# using a comma - separates arguments & inserts a space
 # using concatenation - doesn't insert a space & combines strings
 
-# TYPES we know in Python: String values, Int values, and Float values 
+# Numbers in Python - 2 new TYPES:
+#   INTEGER(int) - whole numbers, no decimal point 
+#   FLOATING POINT NUMBER (float) - decimal number
+result = 0		# integer
+result = 0.0	# float
+print("\nThe starting result is:", result)
+
 # Built-in functions to convert types:
 #   str() converts to a string
 #   int() converts to an int
 #   float() converts to a float
+num1 = input("\nPlease enter your first number: ") # string
+num1 = int(num1)  # integer
 
-num = input("Enter a number: ")  # num stores a string
-num = int(num)                   # num stores an int
-print(num + num)
+# nesting functions: putting one function inside another
+num2 = float(input("Please enter your second number: ")) # float
 
-num2 = float(input("Enter a number: "))    # nesting functions - one inside another
+input("\n\nPress enter to process calculations...")
+print("---------------------------------------")
+# Math Operators:
+#   + , - , / , //, * , ** , % 
 
-print(num2 + num2)
+# addition
+result = num1 + num2
+print(num1, "+", num2, "=", result)  
+# subtraction
+result = num1 - num2
+print(num1, "-", num2, "=", result)	
+# true (floating point) division
+result = num1 / num2
+print(num1, "/", num2, "=", result)	 
+# integer division - leaves off remainder
+result = num1 // num2
+print(num1, "//", num2, "=", result)  
+# multiplication
+result = num1 * num2
+print(num1, "*", num2, "=", result)	 
+# exponents
+result = num1 ** num2
+print(num1, "**", num2, "=", result)   
+# modulus - gives the remainder
+result = num1 % num2
+print(num1, "%", num2, "=", result)
 
-print("The result of", num, "+", num2, "= " + str(num + num2))
+# STATEMENT - a complete thought in a programming language (equivalent to one complete line of code)
+#       e.g. print(2+2)
+# EXPRESSION - code that can be evaluated to a new value (like a math operation)
+#       e.g. 2+2
 
-# CHALLENGE: Ask ask the user for thier name and age and store each in a variable.  Display
-#     the following message in a table: "Name: <user_name>
-#                                        Age:  <user_age>"
-#     Extension: calculate and display how many years until they turn 65 years old
-
-name = input("What is your name? ")
-age = int(input("What is your age? "))
-print("Name:\t\t\t" + name)
-print("Age:\t\t\t" + str(age))
-
-print(name, "is", age, "years old and will retire at age 65 in", 65 - age, "years.")
+print("\nThanks for using the Python calculator. Re-run program for new numbers.")
 
 # !!! do variable tracing programs using slides
- 
