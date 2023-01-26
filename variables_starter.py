75d7255781f2aa9b66f38207e9f3f56514fa35c2
Matt Callaghan <mcallaghan@naperville203.org>
2023-01-26 12:22:39 -0600

    Create variables_starter.py

---
+++ variables_starter.py
@@ -0,0 +1,78 @@
+"""
+Name: 
+Title: Math, Variables, User Input, & Types
+Description: Working with fundametal data types and operations in Python. Storing data in variables. Interacting with a user.
+"""
+
+# VARIABLE:
+
+'''
+#   Follow all variable naming rules and conventions:
+
+# Rules for legal variable names:
+#  - Contain only numbers, letters, and underscores
+#  - Can’t start with a number
+#  - Can’t be a reserved keyword in Python (print, input, type, etc.)
+
+# Guidelines for good variable names:
+#  - Be descriptive of a variable's contents
+#  - Be consistent with formatting
+#  - Follow conventions: 
+#     - names that begin with an underscore have a special meaning
+#     - don’t begin with a capital letter 
+#  - Consider the trade-off of long names
+'''
+
+# ASSIGNMENT STATEMENT: 
+# ASSIGNMENT OPERATOR: 
+
+# INPUT function - 
+
+# CONCATENATION: 
+
+# string repetition
+
+
+# Numbers types in Python - 2 new TYPES:
+#   INTEGER(int) - 
+#   FLOATING POINT NUMBER (float) - 
+
+# Built-in functions to convert types:
+#   str() converts to a string
+#   int() converts to an int
+#   float() converts to a float
+
+# nesting functions:
+
+input("\n\nPress enter to process calculations...")
+print("---------------------------------------")
+num1 = 0
+num2 = 0
+# Math Operators:
+#   + , - , / , //, * , ** , % 
+
+# addition
+result = num1 + num2
+print(num1, "+", num2, "=", result)  
+# subtraction
+
+# true (floating point) division
+
+# integer division - leaves off remainder
+
+# multiplication
+
+# exponents
+ 
+# modulus - gives the remainder
+
+
+ 
+# STATEMENT - 
+# EXPRESSION - 
+
+print("\nThanks for using the Python calculator. Re-run program for new numbers.")
+
+
+
+ 
